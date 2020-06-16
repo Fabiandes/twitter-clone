9be29d823500cf34ed09b8d0799530e4720022f0
Fabian Desnoes <fabiandes@hotmail.co.uk>
2020-06-16 11:45:29 +0100

    Hello Worldgit add .!

--- UserService/server.js
+++ UserService/server.js
@@ -1,4 +1,8 @@
 const app = require('./app');
 const serverPort = require('./keys').serverPort || 6000;
 
+app.get('/',(request, reply)=>{
+    reply.send("Hello World!");
+})
+
 app.listen(serverPort, '0.0.0.0', ()=> console.log("Listening on port " + serverPort));
